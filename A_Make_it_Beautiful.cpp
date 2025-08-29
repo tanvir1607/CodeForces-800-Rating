9e84e29c47cd979b46b6f579306113b1b8dd7109
tanvir1607 <10viranik@gmail.com>
2025-08-29 16:17:24 +0600

    CPP

---
+++ A_Make_it_Beautiful.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() 
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int size;
+        cin >> size;
+        vector<int> vec(size);
+        for (int &x : vec) cin >> x;
+        
+        sort(vec.begin(), vec.end());
+        if (vec.front() == vec.back()) cout << "NO" << endl;
+        else
+        {
+            cout << "YES" << endl << vec.back() << " ";
+            for (int i = 0; i < size - 1; i++) cout << vec[i] << " ";
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
