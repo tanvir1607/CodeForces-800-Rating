8885eb62856bfbb7dc1fe3d63ce682425107268e
tanvir1607 <10viranik@gmail.com>
2025-09-02 16:11:22 +0600

    CPP

--- A_To_Zero.cpp
+++ A_To_Zero.cpp
@@ -25,3 +25,36 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() 
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        int op = 0;
+        if (n & 1) n -= k, op = 1;
+        --k;
+        op += (n + k - 1) / k;
+        cout << op << endl;
+    }
+
+    return 0;
+}
