5e0047d397d094adb94da851ee1ffe15a3f64a8e
tanvir1607 <10viranik@gmail.com>
2025-09-02 15:16:01 +0600

    CPP

--- A_Redstone.cpp
+++ A_Redstone.cpp
@@ -73,3 +73,36 @@ int main()
 
     return 0;
 }
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() 
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int size;
+        cin >> size;
+        vector<int> vec(size);
+        for (int &x : vec) cin >> x;
+        
+        bool found = false;
+        set<int> Set;
+        for (int x : vec)
+            if (Set.count(x)) found = true;
+            else Set.insert(x);
+        cout << (found ? "YES" : "NO") << endl;
+    }
+
+    return 0;
+}
