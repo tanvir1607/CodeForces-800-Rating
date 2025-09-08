ef044266b68d50905061ed54dd51c1d631993a5a
tanvir1607 <10viranik@gmail.com>
2025-09-08 13:36:32 +0600

    CPP

--- A_Unit_Array.cpp
+++ A_Unit_Array.cpp
@@ -1,3 +1,4 @@
+// Approach - 1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -13,17 +14,48 @@ int main()
     cin >> test;
     while (test--)
     {
-        int size, countPos = 0, countNeg = 0, x;
+        int size, pos = 0, neg = 0, op = 0, x;
         cin >> size;
-        while (size--)
-        {
-            cin >> x;
-            x == 1 ? countPos++ : countNeg++;
-        }
-        int operation = 0;
-        while (countPos < countNeg || (countNeg & 1))
-            operation++, countPos++, countNeg--;
-        cout << operation << endl;
+        while (size--) 
+            cin >> x, 
+            x == 1 ? pos++ : neg++;
+
+        while (pos < neg || (neg & 1)) op++, pos++, neg--;
+        cout << op << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+//Approach - 2
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() 
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int size, pos = 0, neg = 0, op = 0, x;
+        cin >> size;
+        while (size--) 
+            cin >> x, 
+            x == 1 ? pos++ : neg++;
+
+        if (pos < neg) op += (neg - pos + 1) / 2, neg -= op;
+        if (neg & 1) op++;
+        cout << op << endl;
     }
 
     return 0;
