f2c17c77d520c3d96e8a91e24f99b0477eb55c22
tanvir1607 <10viranik@gmail.com>
2025-09-08 22:02:23 +0600

    CPP

--- A_Everybody_Likes_Good_Arrays.cpp
+++ A_Everybody_Likes_Good_Arrays.cpp
@@ -1,3 +1,4 @@
+// Approach - 1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -18,9 +19,43 @@ int main()
         vector<int> vec(size);
         for (int &x : vec) cin >> x;
 
-        int parity = vec.front() % 2, ans = 0;
+        int parity = vec.front() & 1, ans = 0;
         for (int i = 1; i < size; i++)
-            vec[i] % 2 == parity ? ans++ : parity ^= 1;
+            (vec[i] & 1) == parity ? ans++ : parity ^= 1;
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+// Approach - 2
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() 
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int size, ans = 0;
+        cin >> size;
+        vector<int> vec(size);
+        for (int &x : vec) cin >> x;
+
+        for (int i = 0; i < size - 1; i++) 
+            ans += !((vec[i] ^ vec[i + 1]) & 1);
 
         cout << ans << endl;
     }
--- A_Extremely_Round.cpp
+++ A_Extremely_Round.cpp
@@ -1,3 +1,40 @@
+// Approach - 1 --> By Loop
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() 
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int num, ans = 0;
+        cin >> num;
+
+        for (int div = 1e5; num; div /= 10)
+            if (num / div) ans += num / div, num = div - 1;
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+// Approach - 2 --> By Math
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -23,74 +60,3 @@ int main()
 
     return 0;
 }
-
-
-
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef long long ll;
-#define endl '\n'
-#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
-
-int main() 
-{
-    optimize();
-
-    int test;
-    cin >> test;
-    while (test--)
-    {
-        int num;
-        cin >> num;
-
-        int ans = 0, div = 1e5;
-        while (num)
-        {
-            if (num / div) ans += num / div, num = div - 1;
-            div /= 10;
-        }
-        cout << ans << endl;
-    }
-
-    return 0;
-}
-
-
-
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef long long ll;
-#define endl '\n'
-#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
-
-int main() 
-{
-    optimize();
-
-    int test;
-    cin >> test;
-    while (test--)
-    {
-        int num;
-        cin >> num;
-
-        int ans = 0, div = 1e5;
-        while (num)
-        {
-            if (num / div)
-            {
-                ans += num / div;
-                ans += log10(div) * 9;
-                break;
-            } 
-            div /= 10;
-        }
-        cout << ans << endl;
-    }
-
-    return 0;
-}
-
-
