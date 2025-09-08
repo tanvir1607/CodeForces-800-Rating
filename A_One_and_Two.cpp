d1f5ce2391e86dc687eebd653366d480ebc7e690
tanvir1607 <10viranik@gmail.com>
2025-09-08 14:55:43 +0600

    CPP

--- A_One_and_Two.cpp
+++ A_One_and_Two.cpp
@@ -1,3 +1,4 @@
+// Approach - 1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -17,7 +18,7 @@ int main()
         cin >> size;
         vector<int> vec(size);
         for (int &x : vec) cin >> x;
-        
+
         int count_2 = count(vec.begin(), vec.end(), 2), ans = 0;
         if (!count_2) ans = 1;
         else if (count_2 & 1) ans = -1;
@@ -27,10 +28,48 @@ int main()
             for (int x : vec)
             {
                 ans++;
-                if (x == 2) found_2++;
-                if (found_2 == count_2 / 2) break;
-            }
+                if (count_2 == 2 * (found_2 += x == 2 ? 1 : 0)) break;
+            }     
         }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+// Approach - 2
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() 
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int size;
+        cin >> size;
+        vector<int> vec(size);
+        for (int &x : vec) cin >> x;
+
+        vector<int> indices;
+        for (int i = 0; i < size; i++) 
+            if (vec[i] == 2) indices.push_back(i + 1);
+
+        int count_2 = count(vec.begin(), vec.end(), 2), ans = 0;
+        if (!count_2) ans = 1;
+        else if (count_2 & 1) ans = -1;
+        else ans = indices[(count_2 / 2) - 1];
         cout << ans << endl;
     }
 
