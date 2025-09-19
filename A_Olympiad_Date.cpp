3c2200a958e5bfa4874c532d0d789377ca815fcf
tanvir1607 <10viranik@gmail.com>
2025-09-20 00:44:28 +0600

    CPP

--- A_Olympiad_Date.cpp
+++ A_Olympiad_Date.cpp
@@ -11,22 +11,21 @@ int main()
     cin >> test;
     while (test--) 
     {
-        int size;
+        int size, ans = 0;
         cin >> size;
         vector<int> vec(size);
         for (int &x : vec) cin >> x;
 
         map<int, int> freq;
-        int ans = 0;
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < size && ans == 0; i++)
         {
             freq[vec[i]]++;
-            if (freq[0] >= 3 && freq[1] >= 1 && freq[2] >= 2 && 
-                freq[3] >= 1 && freq[5] >= 1)
-            {
+            if (freq[0] >= 3 && 
+                freq[1] >= 1 && 
+                freq[2] >= 2 && 
+                freq[3] >= 1 && 
+                freq[5] >= 1)
                 ans = i + 1;
-                break;
-            }
         }
         cout << ans << endl;
     }
